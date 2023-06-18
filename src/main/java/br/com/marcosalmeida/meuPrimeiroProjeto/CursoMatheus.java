f4e0e38d2d4c253d0b1d99938397d0a44674db80
Marcos Antônio de Almeida <marcosantoniomgy@gmail.com>
2023-06-18 18:11:55 -0300

    primeiro commit

---
+++ src/main/java/br/com/marcosalmeida/meuPrimeiroProjeto/CursoMatheus.java
@@ -0,0 +1,20 @@
+package br.com.marcosalmeida.meuPrimeiroProjeto;
+
+import jakarta.websocket.server.PathParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CursoMatheus {
+
+    @GetMapping("/")
+    public String escreverFrase(){
+        return "Vamos estudar!!!";
+    }
+
+    @GetMapping("/somar/{numero1}/{n2}")
+    public int somarDoisNumeros(@PathVariable("numero1") int n1, @PathVariable("n2") int n2){
+        return  n1 + n2;
+    }
+}
---
+++ src/test/java/br/com/marcosalmeida/meuPrimeiroProjeto/MeuPrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcosalmeida.meuPrimeiroProjeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
